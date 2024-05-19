37d603c5cd699efdc16544f41613c66a43a4a960
TTO | TweekZ0 <67276721+Tweek-byte@users.noreply.github.com>
2024-05-20 00:56:27 +0100

    Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+''' Flask setup'''
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    ''' Index function '''
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
