e5ea172159f5d5247c1f3d632a2d5102976d43d7
TTO | TweekZ0 <67276721+Tweek-byte@users.noreply.github.com>
2024-05-20 01:16:27 +0100

    2-c_route.py edit

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+'''Flask startup'''
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''index function'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''return HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """displays a letter "C" and a variable"""
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
